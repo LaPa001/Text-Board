65311c029d2becde40750bf6602d5db7b94f8fdb
MITH Sovanda <vanda.can.read@gmail.com>
2024-06-01 20:00:46 +0700

    Trying to make the canvas full screen

--- web-project/src/Text Board Interface/script.js
+++ web-project/src/Text Board Interface/script.js
@@ -7,18 +7,38 @@ const animeSelect = document.getElementById("animation-style");
 
 // canvas
 const canvas = document.getElementById("scrolltext");
-
 const canvasDimension = canvas.getBoundingClientRect();
-
 const c = canvas.getContext("2d");
-
-c.font = "60px Jolly Lodger";
 c.fillStyle = "#ffffff";
+
+// // full canvas
+// const full_canvas = document.getElementById("full-scrolltext");
+// const fullCanvasDimension = full_canvas.getBoundingClientRect();
+// const full_c = full_canvas.getContext("2d");
+// full_c.fillStyle = "#ffffff";
+// full_c.font = "60px Jolly Lodger";
+
+// full_c.fillText("Hello World", 0, 0);
+// console.log("show something");
 
 // speed input
 const speedInput = document.getElementById("speed");
+let speed;
+
 // size input
 const sizeInput = document.getElementById("size");
+let size;
+
+speedInput.addEventListener("input", (e) => {
+  speed = Number(e.target.value);
+  console.log(speed);
+});
+
+sizeInput.addEventListener("input", (e) => {
+  size = e.target.value;
+  c.font = `${size}px Jolly Lodger`;
+  console.log(size);
+});
 
 let xLeft = 0;
 function leftToRight(text) {
@@ -27,7 +47,7 @@ function leftToRight(text) {
 
   c.fillText(text, xLeft, 100);
 
-  xLeft += 1;
+  xLeft += speed;
 
   if (xLeft > 450) {
     xLeft = -200;
@@ -41,7 +61,7 @@ function rightToLeft(text) {
 
   c.fillText(text, xRight, 100);
 
-  xRight -= 1;
+  xRight -= speed;
 
   if (xRight < -200) {
     xRight = 400;
@@ -50,14 +70,6 @@ function rightToLeft(text) {
 
 textInput.addEventListener("input", (e) => {
   text = e.target.value;
-});
-
-speedInput.addEventListener("input", (e) => {
-  console.log(e.target.value);
-});
-
-sizeInput.addEventListener("input", (e) => {
-  console.log(e.target.value);
 });
 
 animeSelect.addEventListener("change", (e) => {
